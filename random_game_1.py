7ced502c1003a673a9649a52c193ffe0990612dc
Zhiantheprogrammer <zhian.musavir@gmail.com>
2024-07-08 19:30:54 +0530

    Update random_game_1.py

--- random_game_1.py
+++ random_game_1.py
@@ -1,35 +1,20 @@
 import random 
- 
 a=random.randint(1,100) 
 g=0
-print("in this game you have to guess a random number wich is in the range of 1 to 100. You will be getting clues after each number you input as a guess")
-b=int(input("give a guess"))
-if b>100 or b<1:
-    print("the number should be in the range of 1 to 100")
-g=g+1   
-if b==a:
-    print("you found the random number wich was",a,"in 1 try")
-    quit()
+print("in this game you have to guess a random number wich is in the range of 1 to 100.")
+print("You will be getting clues after each number you input as a guess")
 c=(a+10)
 d=(a-10)
 e=(a+5)
 f=(a-5)
-if b>=f and b<=e:
-        print("you are really close")
-else:    
-    if b>=d and b<=c:
-        print("you are close")
-    
-    
-    if b<a:
-        print("try a higher number")
-    if b>a:
-        print("try a lower number")
-
-
 while True:
+    b=(input("give a guess"))
+    try:
+        b=int(b)
+    except:
+        print("please enter a valid number")
+        continue
     g=g+1
-    b=int(input("give a guess"))
     if b>100 or b<1:
         print("the number should be in the range of 1 to 100")
     if (b == a):
@@ -44,4 +29,5 @@ while True:
         print("try a higher number")
     if b>a:
         print("try a lower number")
+     
 print("you found the random number wich was",a,"in ",g,"tries")
